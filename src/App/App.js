6108021a360b85c8d8a4e8bb264020ada1a417cc
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-12-31 12:37:37 -0600

    starter div for app complete

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,12 @@ import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
-
+  render() {
+    return (
+      <div className="App">
+        Hello
+      </div>
+    );
+  }
 }
 export default App;

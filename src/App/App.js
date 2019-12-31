c4ee77c0fe832699158005bf1308b55a0b571530
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-12-31 13:09:36 -0600

    added more cnotent to app

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import studentData from '../helpers/data/studentData';
 
 class App extends React.Component {
+  state = {
+    studentSurvivors: [],
+    deceasedStudents: [],
+  }
+
+  componentDidMount() {
+    const aliveStudents = studentData.livingStudents();
+    this.setState({ studentSurvivors: aliveStudents });
+  }
+
+  killStudent = (studentId) => {
+    studentData.followTheLight(studentId);
+    const deadStudents = studentData.dearlyBeloved();
+    const aliveStudents = studentData.livingStudents();
+    this.setState({ studentSurvivors: aliveStudents });
+    this.setState({ deceasedStudents: deadStudents });
+  }
+
   render() {
     return (
       <div className="App">
-        Hello
+        <div> 
+
+        </div>
       </div>
     );
   }
